fc293dd6efd08f0ffb4f7b5845fb72e855d0fd05
Marc Lynch <lynchmarc2@gmail.com>
2020-06-06 11:07:25 -0700

    init

--- SCRATCH.ts
+++ SCRATCH.ts
@@ -7,9 +7,17 @@ import * as jsonQuery from "json-query";
 import { resolve } from "path";
 
 const pokedex = readJSONSync(resolve("./pokedex.json"));
-
-const card = jsonQuery(`cards[id=${id}]`, {
+// const id = "xy7-4";
+// const supertype = "Pokémon";
+const name = "Mew";
+// const card = jsonQuery(`[id=${id}]`, {
+//   data: pokedex,
+// }).value;
+// const cards = jsonQuery(`[*supertype=${supertype}]`, {
+//   data: pokedex,
+// }).value;
+const cards = jsonQuery(`[*name=${name}]`, {
   data: pokedex,
 }).value;
 
-console.log(card);
+console.log(cards.length);
